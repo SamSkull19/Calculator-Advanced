a89231007fb5bb27063953fb5ac500940e253883
sifat-samin <sifatsamin19@gmail.com>
2025-09-18 21:45:43 +0600

    Calculator Initialized

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./js/*.js"], // Adjust as needed to your HTML and JS files locations
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
